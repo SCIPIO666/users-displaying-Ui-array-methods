46a8c25658ddf26370ad5485a3e7e7c274d3c644
SCIPIO666 <eaphoney@gmail.com>
2025-11-27 15:48:42 +0300

    fetch api

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+async function fetchMultipleUsers(num) {
+ let response = await fetch(`https://randomuser.me/api/?results=${num}`);
+ let data = await response.json();
+ console.log(data.results[0]); 
 
+ return data.results;
 
+ }
+ fetchMultipleUsers(20);
 
+ class user(){
+    constructor
+ }

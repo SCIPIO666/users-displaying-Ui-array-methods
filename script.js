7c74ca13ece83f218027aea1b3a214a810b6d97b
SCIPIO666 <eaphoney@gmail.com>
2025-11-28 10:10:53 +0300

    architecture

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+//////---global variables----////////
+
+const usersContainer=document.getElementsByClassName("users-list");
+
+////////////------  state/data----------  /////////////////
 async function fetchMultipleUsers(num) {
  let response = await fetch(`https://randomuser.me/api/?results=${num}`);
  let data = await response.json();
@@ -6,8 +11,41 @@ async function fetchMultipleUsers(num) {
  return data.results;
 
  }
- fetchMultipleUsers(20);
 
- class user(){
-    constructor
+ fetch()
+// fetchMultipleUsers(20);
+
+////////////////////////////////////////////
+
+////////////////////////---- logic/app functionality rules ---  //////////////////////
+
+
+ /////////////////////////////////////////////////////////////////////////////
+
+
+ ////////////////// ---UI COMPONENTS ---////////////////////////////////////////////
+ class Users{
+    constructor(usersArray){
+        this.usersArray=usersArray;
+        this.parentContainer=
+    }
+
+    addUsers(){
+
+
+    }
+
+    removeUsers(){
+
+    }
+
+    getUser(userId){
+
+    }
+
  }
+
+
+
+
+ ///////////////////////////////////////////////////////////////////////////////
